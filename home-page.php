06c3318c59e4de6fb5d7e6e7b31219a15dfec321
Rortd <62389364+Rortd@users.noreply.github.com>
2020-04-22 18:05:56 +0800

    Create home-page.php

---
+++ home-page.php
@@ -0,0 +1,51 @@
+<?php
+    /*
+    Template Name: Home page
+    */
+    ?>
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
+     <section class="container-fluid aboutbg text-center">
+        <div class="container">
+
+
+<!--aboutme -->
+            
+            <div class="blocker"></div>
+
+      </div><!--  container-->
+    </section>
+
+
+
+
+    <section class="container-fluid ">
+        <div class="container">
+            <div class="row"><!--  a row that gives us access to the BS columns-->
+                <div class="article-title text-center">
+                    <h3>LATEST ARTICLES</h3>
+                </div>
+               <div class="col-md-4 article-cent">
+                   
+<!--col1 -->                 
+ 
+                </div>
+           
+     <div class="col-md-4 article-cent">
+                                      
+<!--col1 -->                 
+              
+  </div>
+  <div class="col-md-4 article-cent">
+                                        
+<!--col1 -->   
+                
+   </div>  
+
+</div><!-- row-->
+</div><!-- container-->
+        
+    </section><!-- container-fluid-->
+
+
+
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
